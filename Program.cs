7da642a077dd03fda2c36d44168998f73b533b25
User <nammiq@example.com>
2023-03-28 00:22:01 +0400

    c#_28.03.2023

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _28._04._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7-e ve 3-e bolunen ededlerin cemi:::
+            //int a = 29;
+            //int b = 100;
+            //int cem = 0;
+            //for ( int i=a; i<=b; i++ )
+            //{
+            //    if ( i%3==0 && i%7==0)
+            //    {
+            //        cem += i;
+            //        Console.WriteLine("7-e ve 3-e bolunen eded: " +i);
+            //    }
+
+            //}
+            //Console.WriteLine("7-e ve 3-e bolunen ededlerin cemi: " +cem);
+
+
+            //Ededin sade ve ya murekkeb olmasini arasdir:::
+            //int a = 103;
+            //int say = 0;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a%i==0 )
+            //    {
+            //        say++;   
+            //    }
+
+            //}
+            //if (say == 2)
+            //{
+            //    Console.WriteLine("eded sadedi:");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded murekkebdii");
+            //}
+
+            //Verilen ededin mertebe sayini tapin:::
+            //int n = 54444;
+            //int sum = 0;
+            //while (n > 0)
+            //{
+            //    n /= 10;
+            //    sum++;
+            //}
+            //Console.WriteLine(sum);
+        }
+    }
+}
